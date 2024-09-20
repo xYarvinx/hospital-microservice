64cfc8cd19da313567ae15a40956c98f573f5e25
Yaroslav <otebator@gmail.com>
2024-09-20 11:17:21 +1000

    init commit

---
+++ src/main/java/com/example/hospitalmicroservice/HospitalMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.hospitalmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hospitalmicroservice/HospitalMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hospitalmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
